f59b9cac4f473cad5f77e6f64534663f0c77e5fc
mug913 <brad.alexander913@gmail.com>
2024-02-19 09:27:42 -0500

    Added Register functionality to APIAgent and required input fields to MainMenu

--- APIAgent/APIAgent.gd
+++ APIAgent/APIAgent.gd
@@ -2,10 +2,9 @@ extends Node
 
 const API = "https://life-grind-be.onrender.com/api/v1"
 
-var email = ""
-var password = ""
 var player_data = {}
 var http_request :  HTTPRequest
+var response_body
 	
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -13,18 +12,30 @@ func _ready():
 	add_child(http_request)
 	http_request.connect("request_completed",Callable(self,"_on_request_completed"))
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
 
-
+# Attempt to Login to backend and if successful, load the response text into var player_data
 func signIn(data):
 	var url = str(API + "/login")
 	var headers = PackedStringArray(["Accept: application/json","Content-Type: application/json"])
 	var json = JSON.stringify(data)
 	http_request.request(url,headers,HTTPClient.METHOD_POST,json)
+	APIAgent.player_data = response_body
+	var scene_tree = get_tree()
+	scene_tree.change_scene_to_file("res://World/World.tscn")
 	
+func signUp(data):
+	var url = str(API + "/users")
+	var headers = PackedStringArray(["Accept: application/json","Content-Type: application/json"])
+	var json = JSON.stringify(data)
+	http_request.request(url,headers,HTTPClient.METHOD_POST,json)
+	APIAgent.player_data = response_body
+	var scene_tree = get_tree()
+	scene_tree.change_scene_to_file("res://World/World.tscn")
+
+# Handle request errors and store response text into Node var for use
 func _on_request_completed(result, response_code, headers, body):
 	if result != 0:
 		print("HTTP request failed with error:", result)
@@ -36,6 +47,5 @@ func _on_request_completed(result, response_code, headers, body):
 		print("headers :" + str(headers))
 		print("body :" + body.get_string_from_utf8())
 		var byte_array_as_string = body.get_string_from_utf8()
-		APIAgent.player_data = JSON.parse_string(byte_array_as_string)
-		var scene_tree = get_tree()
-		scene_tree.change_scene_to_file("res://World/World.tscn")
+		APIAgent.response_body = JSON.parse_string(byte_array_as_string)
+		
--- MainMenu/LogInMenu.gd
+++ MainMenu/LogInMenu.gd
@@ -4,6 +4,7 @@ const API = "https://life-grind-be.onrender.com/api/v1"
 
 var email = "test01@email.com"
 var password = "1234"
+var username = "username"
 var http_request :  HTTPRequest
 
 func _ready():
@@ -17,4 +18,17 @@ func _on_password_box_text_changed():
 
 func _on_log_in_button_pressed():
 	var data = {"user" : {"email": email,"password": password}}
+	$APIAgent.signIn(data)
+	
+func _on_reg_email_box_text_changed():
+	email = $RegisterContainer/EmailBox.text
+
+func _on_reg_password_box_text_changed():
+	password = $RegisterContainer/PasswordBox.text
+	
+func _on_reg_username_box_text_changed():
+	password = $RegisterContainer/UsernameBox.text
+
+func _on_register_button_pressed():
+	var data = {"user" : {"email": email,"password": password, "username": username}}
 	$APIAgent.signIn(data)
