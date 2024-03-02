2a5a1c17ccea338b21f15550752bfde101a2aeaf
mug913 <brad.alexander913@gmail.com>
2024-03-02 18:06:41 -0500

    Added ENV variable to API for test purposes. Modified Area initialization sequence to use popupmenu instead of vboxconts.

--- Area/Area.gd
+++ Area/Area.gd
@@ -1,49 +1,51 @@
+@tool
 extends Node2D
-	
+
 var area_data = {}
 var count = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var text_container = $VBoxContainer
-	var area_menu_name = str(area_data.position) + "_menu"
-	var area_button = _create_area_button("SELECT", area_menu_name)
-	var area_menu = _create_area_menu(area_menu_name)
-	area_button.text = "SELECT"
-	area_button.connect("pressed", func(): _display_area_menu(area_button.get_meta("area_name")))
+	var area_container = $VBoxContainer
+	var area_name = str(area_data.position)
+	area_container.name = area_name + "_area"
+	var area_button = _create_area_button(area_name)
+	#var area_menu = _create_area_menu(area_menu_name)
 	var area_label = Label.new()
 	area_label.text = "AREA " + area_data.name
-	text_container.add_child(area_label)
+	area_label.name = str(area_data.position) + "_label"
+	#_create_area_menu(area_container, area_name)
+	area_container.add_child(area_label)
+	area_container.add_child(area_button)
+
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta):
+func _process(delta):
 	pass
 	
-func _display_area_menu(area_name):
-	var target_area = get_node(area_name)
-	if target_area.visible:
-		target_area.visible = false
-	else:
-		target_area.visible = true
-	count += 1
-	print(str(count))
-	print(target_area.visible)
+#func _create_area_menu(area_container, area_name):
+	#var area_menu = PopupMenu.new()
+	#var menu_title = Control.new()
+	#var menu_item = Label.new()
+	#area_container.add_child(area_menu)
+	#area_menu.name = area_name
+	#area_menu.visible = false
+	#area_menu.name = area_name + "_menu"
+	#menu_title.add_child(menu_item)
+	#area_menu.add_child(menu_title)
+	#return area_menu
 	
-func _create_area_menu(area_name):
-	var area_menu = PopupPanel.new()
-	area_menu.name = area_name
-	var menu_item = Label.new()
-	menu_item.text = "Menu: " + str(area_data.name)
-	area_menu.add_child(menu_item)
-	self.add_child(area_menu)
-	return area_menu
-	
-func _create_area_button(label, area_name):
-	var button = Button.new()
-	button.set_focus_mode(0)
-	button.text = label
-	add_child(button)
-	button.set_meta("area_name", area_name)
+func _create_area_button(area_name):
+	var button = MenuButton.new()
+	button.text = "SELECT"
+	var area_menu_button_name = area_name + "_menu_button"
+	button.name = area_menu_button_name
+	button.get_popup().name = area_name + "_menu"
+	if area_data.subareas.size() != 0:
+		for subarea in area_data.subareas:
+			button.get_popup().add_item(subarea.name)
+			print(subarea.name)
 	return button
 	
 	
